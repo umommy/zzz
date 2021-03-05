40f9de9b6cd1b3b980c7a3ac104c68aa4d4c04c1
umommy <m07irk@yandex.ru>
2021-03-05 22:59:53 +0800

    my commit

--- addgit.cmd
+++ addgit.cmd
@@ -1,11 +1,4 @@
-rem создать ручками github ветку zzz или:
-rem curl -u umommy:113355mw7799 https://api.github.com/user/repos -d'{"name":"test3", "private":"true"}'
-rem git remote add test3 git@github.com:umommy/test3.git
-
-
-git init
+rem дальнейшие обновления
 git add .
-git commit -m "first commit"
-git branch -M main
-git remote add zzz https://github.com/umommy/zzz.git
+git commit -m "my commit"
 git push -u zzz main
